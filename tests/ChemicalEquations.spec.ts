fb002ec9c5472e08135053a66f911ce70031c2b2
nchlsb <nchlsbrdy@gmail.com>
2021-02-21 16:37:27 -0500

    Tests for isBalanced

--- tests/ChemicalEquations.spec.ts
+++ tests/ChemicalEquations.spec.ts
@@ -1,10 +1,10 @@
-import { countElements, makeElement, makeList } from '../src/ChemicalEquations';
+import { countElements, makeElement, makeMolecule, isBalanced } from '../src/ChemicalEquations';
 import { assert, expect } from 'chai';
 import 'mocha';
 
 describe("countElements", () => {
   it("should count 2 hydrogens and 1 oxygen in water", () => {
-    const water = makeList([makeElement('H', 2), makeElement('O')])
+    const water = makeMolecule([makeElement('H', 2), makeElement('O')])
     const result = countElements(water)
     // expect(result.get('H')).to.equal(2)
 
@@ -12,8 +12,8 @@ describe("countElements", () => {
   });
 
   it("should count 2 iron, 3 sulfur, and 12 oxygen in Iron(III) sulfate", () => {
-    const iron3Sulfate = makeList([
-        makeElement('Fe', 2), makeList([makeElement('S'), makeElement('O', 4)], 3)
+    const iron3Sulfate = makeMolecule([
+        makeElement('Fe', 2), makeMolecule([makeElement('S'), makeElement('O', 4)], 3)
     ])
     const result = countElements(iron3Sulfate)
 
@@ -22,5 +22,14 @@ describe("countElements", () => {
         result.get('S') === 3 &&
         result.get('O') === 12
     )
+  });
+})
+
+describe("isBalanced", () => {
+  it("2H_2O -> 2H_2 + O_2", () => {
+    expect(isBalanced({
+      reactants: [[2, makeMolecule([makeElement('H', 2), makeElement('O')])]],
+      products:  [[2, makeElement('H', 2)], [1, makeElement('O', 2)]]
+    })).to.be.true;
   });
 })
