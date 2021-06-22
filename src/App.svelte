903054efa78a5065bd06b7baddb5456d6eb460aa
nchlsb <nchlsbrdy@gmail.com>
2021-06-22 18:44:47 -0400

    New style of GUI with vertical bar chart

--- src/App.svelte
+++ src/App.svelte
@@ -17,25 +17,7 @@
 
 		return retVal
 	}
-/*
-	let reactants: Multiset<ChemicalElement>
-	$: reactants = sumAll(equation.reactants.map(([coefficient, molecule]) => countElements(molecule, coefficient)))
 
-	let products: Multiset<ChemicalElement>
-	$: products = sumAll(equation.products.map(([coefficient, molecule]) => countElements(molecule, coefficient)))
-
-	let inBoth: Multiset<ChemicalElement>
-	$: inBoth = intersection(reactants, products)
-
-	let owedInProducts: Multiset<ChemicalElement>
-	$: owedInProducts = difference(reactants, products)
-    
-	let owedInReactants: Multiset<ChemicalElement>
-	$: owedInReactants = difference(products, reactants)
-
-	let equationIsBalanced: boolean
-	$: equationIsBalanced = isEmpty(owedInReactants) && isEmpty(owedInProducts)
-*/
 	let map: Map<ChemicalElement, Counts>
 	$: map = build(equation)
 
@@ -72,129 +54,57 @@
 </script>
 
 <main>
-	<section id="reactants">
-		{#each equation.reactants as [coefficient, molecule], index}
-			<!-- <button on:click={_ => equation = withReactantCoefficientAtIndex(index, coefficient + 1)}>
-				{#each [...countElements(molecule).elements.entries()] as [chemicalElement, n]}
-				<li>
-					{#each range(n) as _}
-						<svg width={size} height={size}>
-							<circle cx={size / 2} cy={size / 2} r={size / 2.5} stroke="black" stroke-width="3" fill="white"></circle>
-							<text x={size / 2} y={(size * 5) / 8} fill="black" text-anchor="middle">{chemicalElement}</text>
-						</svg>
-					{/each}
-				</li>
+	<table id="reactants-and-prodcuts">
+		<tr>
+			<td class="alignRight">
+				{#each equation.reactants as [coefficient, molecule], index}		
+					{#if index !== 0}
+						<Katex math="+" />
+					{/if}
+					<input type="number" min="1" bind:value={coefficient} on:input={event => {
+						equation = withReactantCoefficientAtIndex(index, parseInt(event.currentTarget.value))
+					}}>
+					
+					<Katex math={toTex(molecule)} />
 				{/each}
-			</button> -->
-
-			{#if index !== 0}
-				<Katex math="+" />
-			{/if}
-			<input type="number" min="1" bind:value={coefficient} on:input={event => {
-				equation = withReactantCoefficientAtIndex(index, parseInt(event.currentTarget.value))
-			}}>
-			
-			<Katex math={toTex(molecule)} />
-		{/each}
-	</section>
-	<section id="arrow">
-		<Katex math={"\\rightarrow"} />
-	</section>
-	<section id="products">
-		{#each equation.products as [coefficient, molecule], index}
-			<!-- <button on:click={_ => equation = withProductCoefficientAtIndex(index, coefficient + 1)}>
-				{#each [...countElements(molecule).elements.entries()] as [chemicalElement, n]}
-				<li>
-					{#each range(n) as _}
-						<svg width={size} height={size}>
-							<circle cx={size / 2} cy={size / 2} r={size / 2.5} stroke="black" stroke-width="3" fill="white"></circle>
-							<text x={size / 2} y={(size * 5) / 8} fill="black" text-anchor="middle">{chemicalElement}</text>
-						</svg>
-					{/each}
-				</li>
+			</td>
+			<td class="alignCenter">
+				<Katex math={"\\rightarrow"} />
+			</td>
+			<td>
+				{#each equation.products as [coefficient, molecule], index}
+		
+					{#if index !== 0}
+						<Katex math="+" />
+					{/if}
+					<input type="number" min="1" bind:value={coefficient} on:input={event =>
+						equation = withProductCoefficientAtIndex(index, parseInt(event.currentTarget.value))
+					}> <Katex math={toTex(molecule)} />
 				{/each}
-			</button> -->
-
-			{#if index !== 0}
-				<Katex math="+" />
-			{/if}
-			<input type="number" min="1" bind:value={coefficient} on:input={event =>
-				equation = withProductCoefficientAtIndex(index, parseInt(event.currentTarget.value))
-			}> <Katex math={toTex(molecule)} />
-		{/each}
-	</section>
-
-	<p>
-		{toStringMap(map)}
-	</p>
-
-	<!--
-	<ul id="owed-in-products"> Owed in products:
-	{#each [...owedInProducts.elements.entries()] as [chemicalElement, n]}
-		<li>
-			{#each range(n) as _}
-				<svg width={size} height={size}>
-					<circle cx={size / 2} cy={size / 2} r={size / 2.5} stroke="black" stroke-dasharray="5,5" stroke-width="3" fill="white"></circle>
-					<text x={size / 2} y={(size * 5) / 8} fill="black" text-anchor="middle">{chemicalElement}</text>
-				</svg>
-			{/each}
-		</li>
-	{/each}
-	</ul>
-
-	<ul id="owed-in-reactants"> Owed in reactants:
-		{#each [...owedInReactants.elements.entries()] as [chemicalElement, n]}
-			<li>
-				{#each range(n) as _}
-					<svg width={size} height={size}>
-						<circle cx={size / 2} cy={size / 2} r={size / 2.5} stroke="black" stroke-dasharray="5,5" stroke-width="3" fill="white"></circle>
-						<text x={size / 2} y={(size * 5) / 8} fill="black" text-anchor="middle">{chemicalElement}</text>
-					</svg>
-				{/each}
-			</li>
-		{/each}
-	</ul>
-
-	<ul id="in-both"> In both:
-		{#each [...inBoth.elements.entries()] as [chemicalElement, n]}
-			<li>
-				{#each range(n) as _}
-					<svg width={size} height={size}>
-						<circle cx={size / 2} cy={size / 2} r={size / 2.5} stroke="black" stroke-width="3" fill="white"></circle>
-						<text x={size / 2} y={(size * 5) / 8} fill="black" text-anchor="middle">{chemicalElement}</text>
-					</svg>
-				{/each}
-			</li>
-		{/each}
-	</ul>
-	-->
-
-	<table>
+			</td>
+		</tr>
 		{#each [...map.entries()] as [element, count]}
-			<tr>
-				<td rowspan="2">{element}</td>
-				<td>{count.amountInProducts}</td>
-				<td>
-					<svg
-						height=10
-						width={Math.max(count.amountInProducts, count.amountInReactants) * 20}
-					>
-						<rect class="products-bar" x=0 y=0 height=10 width={count.amountInProducts * 20}></rect>
-					</svg>
-				</td>
-			</tr>
-			<tr>
-				<!-- td rowspan from the previous tr -->
-				<td>{count.amountInReactants}</td>
-				<td>
-					<svg
-						height=10
-						width={Math.max(count.amountInProducts, count.amountInReactants) * 20}
-					>
-						<rect class="reactants-bar" x=0 y=0 height=10 width={count.amountInReactants * 20}></rect>
-					</svg>
-				</td>
-			</tr>
+		<tr>
+			<td class="alignRight">
+				<svg
+					height=10
+					width={count.amountInReactants * 20}
+				>
+					<rect class="reactants-bar" x=0 y=0 height=10 width={count.amountInReactants * 20}></rect>
+				</svg>
+			</td>
+			<td class="alignCenter">
+				{element}
+			</td>
+			<td>
+				<svg
+					height=10
+					width={count.amountInProducts * 20}
+				>
+					<rect class="products-bar" x=0 y=0 height=10 width={count.amountInProducts * 20}></rect>
+				</svg>
+			</td>
+		</tr>
 		{/each}
 	</table>
 
@@ -224,16 +134,33 @@
 	input[type="number"] {
 		width: 50px;
 	}
+	
+	.reactants-bar {
+		fill: red;
+		text-align: right;
+	}
 
 	.products-bar {
 		fill: blue;
 	}
 
-	.reactants-bar {
-		fill: red;
-	}
-
 	td {
 		text-align: left;
+	}
+
+	.alignRight {
+		text-align: right;
+	}
+
+	.alignCenter {
+		text-align: center;
+	}
+
+	#reactants-and-prodcuts{
+		text-align: center;
+		padding: 1em;
+		margin: 0 auto;
+		width: 100%;
+
 	}
 </style>
